0ae1d9b8920cd4d9d357aad2bddd76324d442e10
Noora Pöysti <noora.poysti@outlook.com>
2021-01-24 01:06:19 +0200

    changed wave to librosa, added speech to text translation in console

--- avatar_speech_to_txt.py
+++ avatar_speech_to_txt.py
@@ -1,6 +1,6 @@
 import argparse
-import wave
-# import numpy as np
+import numpy as np
+import librosa as lb
 from deepspeech import Model
 
 
@@ -12,13 +12,24 @@ def main():
                         help='Path to the output text file (.txt)')
     args = parser.parse_args()
 
-    print('Reading audio file')
-    input_audio = wave.open(args.input, 'rb')
-
     print('Loading model')
     # !!! Define model file here !!!
     ds = Model('deepspeech-0.9.3-models.pbmm')
     desired_sample_rate = ds.sampleRate()
+
+    print('Reading audio file')
+    audio, audio_sample_rate = lb.load(args.input, sr=None)
+
+    if audio_sample_rate != desired_sample_rate:
+        print('Warning: input audio sample rate ({}) is different than the desired sampling rate ({}). '
+              'Resampling might affect speech recognition.'.format(audio_sample_rate, desired_sample_rate))
+        audio = lb.resample(audio, audio_sample_rate, desired_sample_rate)
+
+    # Convert audio from float32 to int16
+    audio = (audio*32768).astype(np.int16)
+
+    # Perform speech to text translation
+    print(ds.stt(audio))
 
 
 if __name__ == '__main__':
