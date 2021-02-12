af465b458f63596c4b4e116200f611916cbbf2ef
Noora <noora.poysti@outlook.com>
2021-02-12 14:18:13 +0200

    added audio listener

---
+++ listen_audio.py
@@ -0,0 +1,21 @@
+import sounddevice as sd
+import soundfile as sf
+
+samplerate = 16000  # Hertz
+duration = 5  # seconds
+filename = 'output.wav'
+
+while(True):
+    choice = input("Press 'r' to record 5 second audio clip or 'q' to exit \n")
+    if choice == 'r':
+        mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
+                channels=2, blocking=True)
+        choice = input("Recording ended, press 's' to save \n")
+        if choice == 's':
+            sf.write(filename, mydata, samplerate)
+
+    elif choice == 'q':
+        break
+
+
+
