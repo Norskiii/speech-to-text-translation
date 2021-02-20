f57d1848085f1a88b4d70b786121513050ecf37f
Noora Pöysti <noora.poysti@tuni.fi>
2021-02-20 22:12:54 +0200

    added while loop that checks input file for changes

--- stt_deepspeech.py
+++ stt_deepspeech.py
@@ -34,40 +34,50 @@ def main():
 
     args = parser.parse_args()
 
-    # file location check
-    if os.path.isfile(args.i):
-        print('Reading audio file')
-        audio, audio_sample_rate = lb.load(args.i, sr=None)
-    else:
-        print('Input audio file not found, exiting')
-        return
-    
     start_time = time.time()
     print('Loading model')
     ds = Model(model_path)
     desired_sample_rate = ds.sampleRate()
-    print('Model loaded in', str(np.round((time.time() - start_time),3)), 'seconds')
+    print('Model loaded in', str(np.round((time.time() - start_time), 3)), 'seconds')
 
     start_time = time.time()
     print('Loading scorer')
     ds.enableExternalScorer(scorer_path)
-    print('Scorer loaded in', str(np.round((time.time() - start_time),3)), 'seconds')
+    print('Scorer loaded in', str(np.round((time.time() - start_time), 3)), 'seconds')
 
-    if audio_sample_rate != desired_sample_rate:
-        print('Warning: input audio sample rate ({}) is different than the desired sampling rate ({}). '
-              'Resampling might affect speech recognition.'.format(audio_sample_rate, desired_sample_rate))
-        audio = lb.resample(audio, audio_sample_rate, desired_sample_rate)
+    prev_modtime = time.time()
+    while True:
+        # File location check
+        if os.path.isfile(args.i):
+            modtime = os.stat(args.i)[8]
+            if modtime == prev_modtime:
+                print('Waiting for changes in input file', end='\r')
+                time.sleep(1)
+            else:
+                prev_modtime = modtime
+                print('\n Reading audio file')
+                start_time = time.time()
+                audio, audio_sample_rate = lb.load(args.i, sr=None)
 
-    # DeepSpeech requires the audio to be type int16
-    audio = (audio*32768).astype(np.int16)
+                if audio_sample_rate != desired_sample_rate:
+                    print('Warning: input audio sample rate ({}) is different than the desired sampling rate ({}). '
+                          'Resampling might affect speech recognition.'.format(audio_sample_rate, desired_sample_rate))
+                    audio = lb.resample(audio, audio_sample_rate, desired_sample_rate)
 
-    start_time = time.time()
-    text = ds.stt(audio)
-    print('Transcription finished in', str(np.round((time.time() - start_time),3)), 'seconds')
+                # DeepSpeech requires the audio to be type int16
+                audio = (audio * 32768).astype(np.int16)
 
-    # Write translation to output file
-    with open(args.o, 'w') as file:
-        file.write(text)
+                start_time = time.time()
+                text = ds.stt(audio)
+                print('Transcription finished in', str(np.round((time.time() - start_time), 3)), 'seconds')
+
+                # Write translation to output file
+                with open(args.o, 'w') as file:
+                    file.write(text)
+
+        else:
+            print('Input audio file not found, exiting')
+            return
 
 
 if __name__ == '__main__':
