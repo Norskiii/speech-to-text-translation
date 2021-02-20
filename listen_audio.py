b35248e7037737d50f83989e80d7242fe4b853be
Noora Pöysti <noora.poysti@tuni.fi>
2021-02-20 21:59:50 +0200

    added while loop that checks if input file has been modified

--- listen_audio.py
+++ listen_audio.py
@@ -7,13 +7,13 @@ import soundfile as sf
 
 samplerate = 16000  # Hertz
 duration = 5  # seconds
-filename = 'output.wav'
+filename = 'stt_input.wav'
 
 while(True):
     choice = input("Press 'r' to record 5 second audio clip or 'q' to exit \n")
     if choice == 'r':
         mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
-                channels=2, blocking=True)
+                channels=1, blocking=True)
         choice = input("Recording ended, press 's' to save \n")
         if choice == 's':
             sf.write(filename, mydata, samplerate)
--- stt_nemo.py
+++ stt_nemo.py
@@ -22,8 +22,6 @@ def main():
     quartznet_path = os.path.join(os.getcwd(),'QuartzNet15x5Base-En.nemo')
     jasper_path = os.path.join(os.getcwd(), 'Jasper10x5Dr-En.nemo')
 
-    # Load model
-    
     parser = argparse.ArgumentParser(description='Translate speech to text and save text to file')
     parser.add_argument('--i', metavar='INPUT', required=True,
                         help='Path to the input audio file (.wav)',
@@ -37,28 +35,37 @@ def main():
 
     args = parser.parse_args()
     
+    # Load correct model
     if args.model == 'quartznet':
         print('Using QuartzNet model')
         model = nemo_asr.models.EncDecCTCModel.restore_from(quartznet_path)
     elif args.model == 'jasper':
         print('Using Jasper model')
         model = nemo_asr.models.EncDecCTCModel.restore_from(jasper_path)
-
-    # file location check
-    if os.path.isfile(args.i):
-        print('Reading audio file')
-        path = [os.path.join(os.getcwd(), args.i)]
-        start_time = time.time()
-        text = model.transcribe(paths2audio_files=path)[0]
-        print('Transcription finished in', str(np.round((time.time() - start_time), 3)), 'seconds')
-    else:
-        print('Input audio file not found, exiting')
-        return
     
-    # Write translation to output file
-    with open(args.o, 'w') as file:
-        file.write(text)
-
+    prev_modtime = time.time()
+    while True:
+        # File location check
+        if os.path.isfile(args.i):
+            modtime = os.stat(args.i)[8]
+            if modtime == prev_modtime:
+                print('Waiting for changes in input file', end='\r')
+                time.sleep(1)
+            else:
+                prev_modtime = modtime
+                print('\n Reading audio file')
+                start_time = time.time()
+                text = model.transcribe(paths2audio_files=[args.i])[0]
+                print('Transcription finished in', str(np.round((time.time() - start_time), 3)), 'seconds')
+                print(text)
+                
+                # Write translation to output file
+                with open(args.o, 'w') as file:
+                    file.write(text)
+        else:
+            print('Input audio file not found, exiting')
+            return
+        
 
 if __name__ == '__main__':
     main()
