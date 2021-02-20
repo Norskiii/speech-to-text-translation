74cd3ebf2e22497c54de90a649eacdea23b17df4
Noora Pöysti <noora.poysti@tuni.fi>
2021-02-20 21:17:34 +0200

    combined jasper and quartznet models to a single file

--- stt_nemo.py
+++ stt_nemo.py
@@ -1,5 +1,5 @@
 #------------------------------------------------------------
-# Speech-to-text translation with pre-trained Jasper model
+# Speech-to-text translation with pre-trained NeMo models
 #------------------------------------------------------------
 
 import nemo
@@ -18,11 +18,11 @@ def extension_check(file, extension, file_use):
 
 
 def main():
-    # Model file location 
-    model_path = os.path.join(os.getcwd(),'Jasper10x5Dr-En.nemo')
+    # Model file locations
+    quartznet_path = os.path.join(os.getcwd(),'QuartzNet15x5Base-En.nemo')
+    jasper_path = os.path.join(os.getcwd(), 'Jasper10x5Dr-En.nemo')
 
     # Load model
-    jasper = nemo_asr.models.EncDecCTCModel.restore_from(model_path)
     
     parser = argparse.ArgumentParser(description='Translate speech to text and save text to file')
     parser.add_argument('--i', metavar='INPUT', required=True,
@@ -31,18 +31,26 @@ def main():
     parser.add_argument('--o', metavar='OUTPUT', required=True,
                         help='Path to the output text file (.txt)',
                         type=lambda f: extension_check(f, '.txt', 'Output'))
+    parser.add_argument('-jasper', dest='model', action='store_const',
+                        const='jasper', default='quartznet',
+                        help='Use Jasper speech recognition model (default: QuartzNet)')
 
     args = parser.parse_args()
     
+    if args.model == 'quartznet':
+        print('Using QuartzNet model')
+        model = nemo_asr.models.EncDecCTCModel.restore_from(quartznet_path)
+    elif args.model == 'jasper':
+        print('Using Jasper model')
+        model = nemo_asr.models.EncDecCTCModel.restore_from(jasper_path)
 
     # file location check
     if os.path.isfile(args.i):
         print('Reading audio file')
         path = [os.path.join(os.getcwd(), args.i)]
-
         start_time = time.time()
-        text = jasper.transcribe(paths2audio_files=path)[0]
-        print('Transcription finished in', str(np.round((time.time() - start_time),3)), 'seconds')
+        text = model.transcribe(paths2audio_files=path)[0]
+        print('Transcription finished in', str(np.round((time.time() - start_time), 3)), 'seconds')
     else:
         print('Input audio file not found, exiting')
         return
@@ -50,7 +58,7 @@ def main():
     # Write translation to output file
     with open(args.o, 'w') as file:
         file.write(text)
-    
+
+
 if __name__ == '__main__':
     main()
-    
