7215edffddc949d33a3a857762a06e510ff46be0
Noora Pöysti <noora.poysti@outlook.com>
2021-02-25 13:46:12 +0200

    added setup file

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Install packages required by Nemo Toolkit
+apt-get update && apt-get install -y libsndfile1 ffmpeg
+
+# Install pip requirements
+pip3 install -r requirements.txt
+
+# Get pre-trained models
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer
+wget https://api.ngc.nvidia.com/v2/models/nvidia/nemospeechmodels/versions/1.0.0a5/files/Jasper10x5Dr-En.nemo
+wget https://api.ngc.nvidia.com/v2/models/nvidia/nemospeechmodels/versions/1.0.0a5/files/QuartzNet15x5Base-En.nemo
+
